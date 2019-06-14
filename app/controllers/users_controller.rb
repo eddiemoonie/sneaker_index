ed9a23c26f280194ebe8ae75fa7e65f02f14f0b4
github username <github email address>
2019-06-14 19:04:19 -0400

    user validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  require 'pry'
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def show
@@ -11,8 +12,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      if params[:password] == params[:password_confirmation]
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      end
     else
       redirect_to new_user_path
     end
@@ -45,5 +48,5 @@ class UsersController < ApplicationController
         :password_confirmation
       )
     end
-    
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :username, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, confirmation: { case_sensitive: true }
 end
