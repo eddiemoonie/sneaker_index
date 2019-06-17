98b44cb348ce69c06811592ceda1b031aa50392a
github username <github email address>
2019-06-17 17:05:08 -0400

    fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,25 +7,6 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.find_by(:username => params[:user][:username])
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-    else
-      redirect_to login_path
-    end
-  end
-
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_path
-  end
-  
   private
     def require_logged_in
       redirect_to root_path unless logged_in?
